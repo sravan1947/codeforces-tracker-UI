c762effadd6bb297c89bd40926778ace99811325
syed <syed.taha47@gmail.com>
2021-01-31 22:19:38 +0530

    update

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/con-fusion'));
+app.use(express.static(__dirname + '/dist/conFusion'));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+
-    '/src/index.html'));});
-app.listen(process.env.PORT || 8080);
+    '/dist/conFusion/index.html'));});
+var port=process.env.PORT || 8080;
+app.listen(port);
